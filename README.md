a841d690fc209503be74e1074a8a4e259819dfc2
qwertuhh <jainneema361@gmail.com>
2024-07-01 20:57:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# OnlyChat
+
+Welcome to my project repository! This project showcases the various programming concepts and techniques I have learned. The primary focus is on understanding and implementing these concepts rather than perfecting the design or creating production-ready solutions.
+
+OnlyChat is a simple chat application that leverages an external API to provide real-time messaging capabilities.
+
+[👉click here to hope on site!](https://qwertuhh-alarm-clock.netlify.app/)[click here to hope on site!](https://qwertuhh-onlychat.netlify.app/)
+
+## What i had learnt
+
+- API
+- Loader
+- HTML object
+
+## Acknowledgements
+
+Thank you for reviewing my project. This repository is a reflection of my learning journey, and I appreciate any feedback or suggestions for improvement.
+
+Feel free to explore the code, review the documentation, and run the tests to see the concepts in action.
---
+++ script.js
@@ -0,0 +1,46 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const promptForm = document.getElementById("prompt-form");
+const API_KEY = "AIzaSyAwfN_uJYTFcaXd1f4o09uy2QIEnC15J6w";
+const genAI = new GoogleGenerativeAI(API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const mainBox = document.getElementsByTagName("main")[0];
+
+const outputBox = (box, text) => {
+  box.className = "output";
+  box.innerHTML = marked.parse(text);
+};
+const loader = () => {
+  let output = document.createElement("div");
+  output.className = "output";
+  output.innerHTML = ` <span class="loader"></span>`;
+  output.className = "onloader";
+  mainBox.appendChild(output);
+  return output;
+};
+const inputBox = (text) => {
+  let input = document.createElement("div");
+  input.className = "input";
+  input.innerHTML = marked.parse(text);
+  mainBox.appendChild(input);
+};
+
+
+// * Main function 
+
+async function run(prompt) {
+  const outBox = loader();
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  outputBox(outBox, text);
+}
+
+// * Event Listener
+
+promptForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  prompt = promptForm[0].value;
+  inputBox(prompt);
+  run(prompt);
+});
